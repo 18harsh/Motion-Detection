15d56d02cf1a8fbcc571c7aa58ec75ed5582748c
Harsh Gandhi <18harshgandhi@gmail.com>
2020-08-01 13:50:16 +0530

    object detection

--- motionDetector.py
+++ motionDetector.py
@@ -1,10 +1,23 @@
-import cv2, time, pandas
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+import tensorflow as tf
+import pandas
+import seaborn
+import time
 from datetime import datetime
+
 
 first_frame=None
 status_list=[None,None]
 times=[]
 df=pandas.DataFrame(columns=["Start","End"])
+# model = keras.models.load_model('saved_models/MY-cifar10-trained-model.h5')
+model = keras.models.load_model('../project02/saved_models/keras_fashionshape(28,28,1)-trained-model.h5') 
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+# category = ["Airplanes"," Cars" ,"Birds" ,"Cats"," Deer", "Dogs", "Frogs", "Horses ","Ships"," Trucks"]
+
 
 video=cv2.VideoCapture(0)
 
@@ -42,17 +55,46 @@ while True:
         times.append(datetime.now())
 
 
-    cv2.imshow("Gray Frame",gray)
-    cv2.imshow("Delta Frame",delta_frame)
-    cv2.imshow("Threshold Frame",thresh_frame)
+    # cv2.imshow("Gray Frame",gray)
+    # cv2.imshow("Delta Frame",delta_frame)
+    # cv2.imshow("Threshold Frame",thresh_frame)
     cv2.imshow("Color Frame",frame)
 
+    #Machine Learning
+
+    # img_array = cv2.resize(frame,(32,32))
+    # img_array = img_array.astype('float32')
+    # img_array = img_array/255
+    # img_array = img_array.reshape(-1,32,32,3)
+    # val = model.predict_classes(img_array)
+    # print(category[np.array(val)[0]])
+    
+    def get_image(img):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return cv2.resize(img,(28,28))
+
+    try:
+        # crop_img = gray[y:y+h, x:x+w]
+        images = np.array([get_image(frame)])
+        images_reshaped = images.reshape(images.shape[0], 28, 28, 1)
+        images_reshaped = tf.cast(images_reshaped, tf.float32)
+        preds = model.predict(images_reshaped)
+        if 100*np.max(preds)>=80:
+            predicted_label = np.argmax(preds[0])
+            print(class_names[predicted_label])
+    except Exception as e:
+        print(e)
+
+
+
+    #Quit
     key=cv2.waitKey(1)
 
     if key==ord('q'):
         if status==1:
             times.append(datetime.now())
         break
+
 
 print(status_list)
 print(times)
@@ -64,3 +106,6 @@ df.to_csv("Times.csv")
 
 video.release()
 cv2.destroyAllWindows
+plt.imshow(images[0])
+# plt.imshow(crop_img)
+plt.show()
